ae323b61b38fe61e3ce2b3e5e36a0572964d2c17
garipfederico <garip.federico@gmail.com>
2022-07-15 10:03:54 -0300

    EmpiezaLimpieza

--- src/App.js
+++ src/App.js
@@ -40,8 +40,9 @@ function App() {
       <BrowserRouter >
         <Drawer showMenu={true} token={token} />
         <Routes >
-          <Route path="/" element={<h1>Logeado</h1>} >
 
+          <Route path="/" element={<h1>Logeado</h1>} >
+          </Route>
           {/* {token.permisos.includes("gestion-clientes") &&
             <Route path="/clientes" element={<ClientsConsultPage />} >
               <Route path=":clienteId" element={<ClientPage />} />
@@ -49,16 +50,14 @@ function App() {
           } */}
 
 
-          <Route path="clientes" element={<ClientsPage />} >
-          <Route path=":clienteId" element={<ClientPage />} >
-          </Route>
+          <Route path="/clientes" element={<ClientsPage />} />
 
-          </Route>
+          <Route path="/clientes/:clienteId" element={<ClientPage />} />
+
 
           {token.permisos.includes("gestion-planes") && <Route path="/planes" element={<h1>Gestion de planes</h1>} />}
           {token.permisos.includes("mis-planes") && <Route path="/mis-planes" element={<ClientPage />} />}
 
-          </Route>
           <Route path="/*" element={<h1>Error no autorizado</h1>} />
         </Routes>
         <Footer />
