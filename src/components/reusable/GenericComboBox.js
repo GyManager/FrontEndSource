dde50dfaae5facfa88a39b45aff39ae82af5752f
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-07-22 19:55:07 -0300

    reusable and configurable ComboBox

---
+++ src/components/reusable/GenericComboBox.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function GenericComboBox(props) {
+
+    return (
+        <div>
+            <FormControl sx={{ m: 1, minWidth: 200 }} >
+
+                <InputLabel id={props.label}>{props.label}</InputLabel>
+
+                <Select
+                    labelId={props.label}
+                    value={props.value}
+                    onChange={props.handleChange}
+                    autoWidth
+                    label={props.label}
+                    readOnly={props.editable? false: true }
+                >
+                    <MenuItem value={props.valueForNone}>{props.labelForNone}</MenuItem>
+                    { props.values.map(value => (<MenuItem value={value}>{value}</MenuItem>)) }
+                </Select>
+
+            </FormControl>
+        </div>
+    );
+}
