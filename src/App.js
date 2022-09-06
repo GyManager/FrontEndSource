528c123ab4d9d726f70ea7e3c5f7bdeaec52ac12
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-09-06 17:54:01 -0300

    Theme

--- src/App.js
+++ src/App.js
@@ -28,55 +28,63 @@ import SnackbarSystem from './components/reusable/SnackbarSystem';
 import { SnackbarProvider } from './context/SnackbarContext';
 import ErrorModalSystem from './components/reusable/ErrorModalSystem';
 import { ErrorProvider } from './context/ErrorContext';
+import gymanagerTheme from './GymanagerTheme';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 function App() {
 
   const token = AuthService.getStoredSession();
 
+  const theme = createTheme(gymanagerTheme);
+
   if (!token) {
     return (
-      <div className="fondo" >
-        <BrowserRouter>
-          <Drawer showMenu={false} />
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/*" element={<Navigate to="/login" />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="fondo" >
+          <BrowserRouter>
+            <Drawer showMenu={false} />
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/*" element={<Navigate to="/login" />} />
+            </Routes>
+            <Footer />
+          </BrowserRouter>
+        </div>
+      </ThemeProvider>
     )
   }
 
   return (
-    <DataProvider>
-      <ErrorProvider>
-        <SnackbarProvider>
-          <div className="fondo">
-            <BrowserRouter >
-              <Drawer showMenu={true} token={token} />
-              <Routes >
-                <Route path="/" element={<h1>Logeado</h1>} />
+    <ThemeProvider theme={theme}>
+      <DataProvider>
+        <ErrorProvider>
+          <SnackbarProvider>
+            <div className="fondo">
+              <BrowserRouter >
+                <Drawer showMenu={true} token={token} />
+                <Routes >
+                  <Route path="/" element={<h1>Logeado</h1>} />
 
-                {token.permisos.includes("gestion-clientes") && <Route path="/clientes" element={<ClientsPage />} />}
-                {token.permisos.includes("gestion-clientes") && <Route path="/clientes/:clienteId" element={<ClientPage />} />}
-                {token.permisos.includes("gestion-planes") && <Route path="/clientes/:clienteId/planes/:idPlan" element={<PlanPage />} />}
-                {token.permisos.includes("gestion-micro-planes") && <Route path="/micro-planes" element={<MicroPlanesPage/>} />}
-                {token.permisos.includes("gestion-micro-planes") && <Route path="/micro-planes/:idMicroPlan" element={<MicroPlanPage/>} />}
-                {token.permisos.includes("mis-planes") && <Route path="/mis-planes" element={<UnderConstructionPage title='Mis planes'/>} />}
-                {token.permisos.includes("gestion-ejercicios") && <Route path="/ejercicios" element={<EjerciciosPage/>} />}
-                {token.permisos.includes("gestion-ejercicios") && <Route path="/ejercicios/:idEjercicio" element={<UnEjercicioPage/>} />}
+                  {token.permisos.includes("gestion-clientes") && <Route path="/clientes" element={<ClientsPage />} />}
+                  {token.permisos.includes("gestion-clientes") && <Route path="/clientes/:clienteId" element={<ClientPage />} />}
+                  {token.permisos.includes("gestion-planes") && <Route path="/clientes/:clienteId/planes/:idPlan" element={<PlanPage />} />}
+                  {token.permisos.includes("gestion-micro-planes") && <Route path="/micro-planes" element={<MicroPlanesPage/>} />}
+                  {token.permisos.includes("gestion-micro-planes") && <Route path="/micro-planes/:idMicroPlan" element={<MicroPlanPage/>} />}
+                  {token.permisos.includes("mis-planes") && <Route path="/mis-planes" element={<UnderConstructionPage title='Mis planes'/>} />}
+                  {token.permisos.includes("gestion-ejercicios") && <Route path="/ejercicios" element={<EjerciciosPage/>} />}
+                  {token.permisos.includes("gestion-ejercicios") && <Route path="/ejercicios/:idEjercicio" element={<UnEjercicioPage/>} />}
 
-                <Route path="/*" element={<NoAutorizadoPage/>} />
-              </Routes>
-              <Footer />
-            </BrowserRouter>
-            <SnackbarSystem/>
-            <ErrorModalSystem/>
-          </div>
-        </SnackbarProvider>
-      </ErrorProvider>
-    </DataProvider>
+                  <Route path="/*" element={<NoAutorizadoPage/>} />
+                </Routes>
+                <Footer />
+              </BrowserRouter>
+              <SnackbarSystem/>
+              <ErrorModalSystem/>
+            </div>
+          </SnackbarProvider>
+        </ErrorProvider>
+      </DataProvider>
+    </ThemeProvider>
   );
 }
 
