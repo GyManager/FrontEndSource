60513444cac1ce5abdb2218f8959c0f4cb7c5352
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-09-07 14:34:05 -0300

    arreglando botones y fabs

--- src/components/reusable/ButtonToFabCrear.js
+++ src/components/reusable/ButtonToFabCrear.js
@@ -8,27 +8,8 @@ export default function ButtonToFabCrear(props){
 
     return (
         <Fragment>
-            <Button
-                sx={{display: { xs: 'none', md: 'inline-flex' }}}
-                variant='contained'
-                startIcon={<Add/>}
-                size='medium'
-                onClick={props.onClick}
-            >
-                {props.label}
-            </Button>
-            <Box
-                sx={{
-                    display: { xs: 'block', md: 'none'}, 
-                    position: 'fixed',
-                    right: '4vw',
-                    bottom: '4vh'
-                }}
-            >
-                <Fab color='primary' aria-label='add' onClick={props.onClick}>
-                    <Add/>
-                </Fab>
-            </Box>
+
+
         </Fragment>
     )
 }
