e3b5c7a75ff7be2c805d16fce0dd9cd5b8e79669
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-09-30 21:43:10 -0300

    prettier y fix

--- src/components/user/SeccionRoles.js
+++ src/components/user/SeccionRoles.js
@@ -1,10 +1,3 @@
-import usersService from "../../services/users.service";
-
-import Chip from "@mui/material/Chip";
-import Stack from "@mui/material/Stack";
-import { MultipleSelectChipV2 } from "../reusable";
-import { Grid, Typography } from "@mui/material";
-
 import React, { useEffect, useState } from 'react';
 
 import usersService from '../../services/users.service'
