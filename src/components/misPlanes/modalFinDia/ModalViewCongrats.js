b7d3eaf208f62a68a529c8e22d27f2122753120e
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-10-17 13:48:52 -0300

    defecto - No se muestran errores de unique

---
+++ src/components/misPlanes/modalFinDia/ModalViewCongrats.js
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material";
+import { Fragment } from "react";
+
+export default function ModalViewCongrats() {
+    return (
+        <Fragment>
+            <Typography variant="h5" align="center">
+                FELICITACIONES!!! COMPLETASTE UN DIA MAS DE ENTRENAMIENTO!!
+            </Typography>
+            <br />
+            <Typography variant="h5" align="center">
+                SEGUI ASI PARA ALCANZAR TUS OBJETIVOS!!
+            </Typography>
+        </Fragment>
+    );
+}
